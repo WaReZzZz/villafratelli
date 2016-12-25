7792bcfa161f3393c322d09df3831805dc21dc01
Afriat Yaniv <yaniv.afriat@gmail.com>
2016-12-25 22:23:50 +0100

    Init projet

---
+++ src/VillaFratelli/Config/development.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace VillaFratelli\Config;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+return [
+    'BDD' => [
+        'DBHOST' => 'localhost',
+        'DBNAME' => 'fratelli',
+        'DBUSER' => 'root',
+        'DBPASS' => 'jjZcWmZDQBT8Hfsn'
+    ]
+];

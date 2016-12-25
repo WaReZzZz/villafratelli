2a7fa0e268749ec4c922952ad90f9cf734aeca19
Afriat Yaniv <yaniv.afriat@gmail.com>
2016-12-25 22:23:50 +0100

    Init projet

---
+++ src/VillaFratelli/Controllers/HomeControllerProvider.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace VillaFratelli\Controllers;
+
+use Silex\Application;
+use Silex\Api\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeControllerProvider implements ControllerProviderInterface
+{
+
+    protected $conf;
+
+    public function connect(Application $app)
+    {
+        // creates a new controller based on the default route
+        $controllers = $app['controllers_factory'];
+        $globalConf = include __DIR__ . '/../Config/config.php';
+        $envConf = array();
+        if (file_exists(__DIR__ . '/../Config/' . getenv('APPLICATION_ENV') . '.php')) {
+            $envConf = include_once __DIR__ . '/../Config/' . getenv('APPLICATION_ENV') . '.php';
+        }
+        $this->conf = array_merge($globalConf, $envConf);
+        $controllers->before(function () use ($app) {
+            $app['db.options'] = array(
+                'driver' => 'pdo_mysql',
+                'host' => $this->conf['BDD']['DBHOST'],
+                'dbname' => $this->conf['BDD']['DBNAME'],
+                'user' => $this->conf['BDD']['DBUSER'],
+                'password' => $this->conf['BDD']['DBPASS'],
+                'charset' => 'utf8');
+        });
+
+        $controllers->get('/', function () use ($app) {
+            return $app['twig']->render('home.twig');
+        })->bind('homepage');
+
+        $controllers->get('/description/', function () use ($app) {
+            return $app['twig']->render('description.twig');
+        })->bind('description');
+
+        $controllers->get('/rooms/', function () use ($app) {
+            $images = $this->getImages($app, array("dossier = 'images/big/rooms/'"));
+            return $app['twig']->render('images.twig', ['aImages' => $images, 'page' => 'rooms']);
+        })->bind('rooms');
+
+        $controllers->get('/spa/', function () use ($app) {
+            $images = $this->getImages($app, array("dossier = 'images/big/spa/'"));
+            return $app['twig']->render('images.twig', ['aImages' => $images, 'page' => 'spa']);
+        })->bind('spa');
+
+        $controllers->get('/pool/', function () use ($app) {
+            $images = $this->getImages($app, array("dossier = 'images/big/piscine/'"));
+            return $app['twig']->render('images.twig', ['aImages' => $images, 'page' => 'piscine']);
+        })->bind('pool');
+
+        $controllers->get('/night/', function () use ($app) {
+            $images = $this->getImages($app, array("dossier = 'images/big/nuit/'"));
+            return $app['twig']->render('images.twig', ['aImages' => $images, 'page' => 'nuit']);
+        })->bind('night');
+
+        $controllers->get('/contacts/', function () use ($app) {
+            return $app['twig']->render('contacts.twig', ['page' => 'contacts']);
+        })->bind('contacts');
+
+        //mardi mercredi samedi lundi jeudi vendredi
+        return $controllers;
+    }
+
+    private function getImages($app, $where = null, $or = null, $else = null)
+    {
+        $sqlWhere = '';
+        if (is_array($where)) {
+            $sqlWhere = " where (" . implode(' and ', $where) . ")";
+        }
+        $sqlOr = '';
+        if (is_array($or)) {
+            $sqlOr = " OR (" . implode(' and ', $or) . ")";
+        }
+        $sqlElse = '';
+        if (is_array($else)) {
+            $sqlElse = " OR (" . implode(' and ', $else) . ")";
+        }
+        $sql = "SELECT * FROM images $sqlWhere $sqlOr $sqlElse ";
+        $result = $app['db']->fetchAll($sql);
+        return $result;
+    }
+
+}
---
+++ web/index.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/VillaFratelli/Config/config.php';
+
+use Silex\Application\SwiftmailerTrait;
+
+$app = new Silex\Application();
+
+if (null !== getenv('APPLICATION_ENV') && getenv('APPLICATION_ENV') === 'development') {
+    $app['debug'] = true;
+} else {
+    $app['debug'] = false;
+}
+
+$app->register(new Silex\Provider\SessionServiceProvider(), array(
+    'session.storage.options' => array(
+        'cookie_lifetime' => 14400, //4x60minutes
+        'cookie_secure' => !$app['debug'],
+        'cookie_httponly' => !$app['debug']
+    )
+));
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'orm.proxies_dir' => __DIR__ . '/../var/doctrine/proxies'));
+
+$app->register(new Silex\Provider\SwiftmailerServiceProvider(), array(
+    'swiftmailer.options' => array(
+        'host' => 'auth.smtp.1and1.fr',
+        'port' => '465',
+        'username' => 'contact@textile.sexy',
+        'password' => '-y8d+^QH"F)|',
+        'encryption' => 'ssl',
+        'auth_mode' => null,
+    )
+));
+$app['mailer'] = new \Swift_MailTransport;
+
+//$app['autoloader']->registerNamespace('TextileSexy\Controller', __DIR__.'/../lib');
+//$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../src/VillaFratelli/Views',
+    'twig.options' => array(
+        'cache' => (!$app['debug']) ? __DIR__ . '/../cache/twig' : false,
+    )
+));
+//$app['pages'] = include_once __DIR__ . '/../src/TextileSexy/Config/pages.global.php';
+$app['twig'] = $app->extend("twig", function (\Twig_Environment $twig, Silex\Application $app) {
+    //$twig->addExtension(new Twig_Extensions_Extension_Text());
+    $twig->addExtension(new \VillaFratelli\Services\MyTwigExtensions());
+
+    return $twig;
+});
+
+$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
+    'http_cache.cache_dir' => __DIR__ . '/../cache/',
+));
+
+$app->error(function (\Exception $e, Symfony\Component\HttpFoundation\Request $request, $code) use($app) {
+    $message = \Swift_Message::newInstance('Error on Textile.sexy', $request . 'Exception : ' . $e)
+            ->setFrom('contact@textile.sexy')
+            ->setTo(array('yaniv.afriat@gmail.com'));
+    $app['mailer']->send($message);
+    echo $e;
+});
+$app->mount('/', new \VillaFratelli\Controllers\HomeControllerProvider());
+$app->mount('/ajax', new \VillaFratelli\Controllers\AjaxControllerProvider());
+
+$app->run();

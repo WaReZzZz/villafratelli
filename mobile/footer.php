55ffaf7045ecb6853a832eb73ab61cac18817ce9
Afriat Yaniv <yaniv.afriat@gmail.com>
2017-01-08 01:37:50 +0100

    reviews sitemap

--- mobile/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-<div data-role="footer">
-<h5 class="copyright">Copyright &copy; <?php echo date('Y') ?> www.villafratelli.com<br />
-                    Tous droits r&eacute;serv&eacute;s. </h5></div>
-
-	</div>
-</body>
-
-</html>
